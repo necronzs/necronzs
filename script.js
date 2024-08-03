27d4c539aaa89cb73983b022e80fb819d87c1d1d
kimgamerTV <jobatjame1@gmail.com>
2024-08-03 14:40:21 +0700

    uwu

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.title = "Redirecting...";
+    console.log("Redirecting....");
+    setTimeout(() => {
+        window.location.href = "https://senkosan.is-cute.today/s/zh8komngk8";
+    }, 3000);
+}
